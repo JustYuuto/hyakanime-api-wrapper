d9bdbf72fa49e5fb106afb8b9ff84dc87d5c825f
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-08-07 01:02:26 +0200

    types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,22 @@
-export interface User {
-  isPremium: boolean,
-  photoURL: string,
-  themeColor: number,
-  username: string
+declare interface API {
+  user: UserAPI
 }
+
+declare interface UserAPI {
+  basicInformation: (username: string) => Promise<User>,
+  completeInformation: (token: string) => Promise<User & {
+    email: string,
+    biographie: string,
+    NotificationToken: string
+  }>,
+}
+
+export interface User {
+  photoURL: string,
+  username: string,
+  isPremium: boolean,
+  themeColor: number
+}
+
+declare const api: API;
+export default api;
